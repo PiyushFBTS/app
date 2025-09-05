5bf03b2eb5874758404d390b870a8440ef1345eb
PiyushFBTS <piyush.malviya@fbts.in>
2025-09-05 12:40:40 +0530

    feat/dashboard-chart

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -4,6 +4,7 @@ import { useSelector } from "react-redux"
 import type { RootState } from "@/store"
 import ItemCard from "@/components/dashboard/item-card/page"
 import { ItemPieChart } from "@/components/dashboard/item-pie-chart/page"
+import { OutletwiseOrders } from "@/components/dashboard/OutletwiseOrders/page"
 
 function Dashboard() {
   const user = useSelector((state: RootState) => state.user)
@@ -11,13 +12,13 @@ function Dashboard() {
   return (
     <>
       <div className="space-y-6">
-        <div>
+        <div className="p-4">
           <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
           <p className="text-muted-foreground">Welcome to Third Wave Coffee Management System</p>
         </div>
 
         {/* Display user information */}
-        <div className="bg-card p-4 rounded-lg border">
+        <div className="bg-card m-4 p-4 rounded-lg border">
           <div className="space-y-1">
             <p>
               <strong> Hello ,</strong> {user.first_name} {user.last_name}
@@ -27,8 +28,16 @@ function Dashboard() {
         <div>
           <ItemCard />
         </div>
-        <div>
-          <ItemPieChart />
+        <div className="grid gap-4 md:grid-cols-2 w-full p-2 m-4">
+          {/* Left side */}
+          <div>
+            {/* <ItemPieChart /> */}
+          </div>
+
+          {/* Right side */}
+          <div>
+            <OutletwiseOrders />
+          </div>
         </div>
       </div>
     </>
---
+++ src/types/dashboard.type.ts
@@ -0,0 +1,11 @@
+export type OrdersList = {
+    col1: string
+    col2: string
+    col3: number
+}
+
+export type OrderTotal = {
+    col1: string
+    col2: number
+    icon?:any
+}
