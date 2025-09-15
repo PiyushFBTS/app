a459bafff0bc8b757b5ed9835a992ae2a3eeb966
PiyushFBTS <piyush.malviya@fbts.in>
2025-09-15 18:50:05 +0530

    fix-typo

--- src/app/api/pos/fetchedByPos/route.ts
+++ src/app/api/pos/fetchedByPos/route.ts
@@ -9,34 +9,36 @@ export async function PUT(request: NextRequest) {
             return NextResponse.json({ error: "Database connection not available" }, { status: 500 })
         }
 
-        const { order_id } = body
-        
+        const { order_id } = body;
+
         if (!Array.isArray(order_id) || order_id.length === 0) {
             return NextResponse.json({ message: "No IDs provided" }, { status: 400 });
         }
 
         const currentTime = new Date();
-        const processed ='Y'
+        const processed = "Y";
+
         const query = `
-            UPDATE "OOMiddleware".order_header SET
-            processed_at_pos = $1,
-            processed_at_pos_time =$2
-            WHERE order_id = ANY($3::bigint[])
-          `;
+        UPDATE "OOMiddleware".order_header
+        SET fetched_by_pos = $1,
+            "fetched_by_pos_Time" = $2
+        WHERE order_id = ANY($3::bigint[])
+        `;
 
-        const values = [
-             processed,currentTime, order_id
-        ]
+        const values = [processed, currentTime, order_id];
 
-        await pool.query(query, values)
+        await pool.query(query, values);
 
-        return NextResponse.json({ message: "processed at pos successfully" }, { status: 200 })
+        return NextResponse.json(
+            { message: "Fetched at POS successfully" },
+            { status: 200 }
+        );
 
 
     } catch (error) {
-        console.error("Error in POST:", error);
+        console.error("Error in fetched by pos:", error);
         return NextResponse.json(
-            { error: "Failed to fethc ay POS" },
+            { error: "Fail to Fetched At POS" },
             { status: 500 }
         );
     }
